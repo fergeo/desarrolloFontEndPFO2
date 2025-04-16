58ed85d532f5793b6f738d2240ea437fe56c65b6
fergeo <fergeo@yahoo.com>
2025-04-16 14:22:08 -0300

    Modo Oscuro
    Se agrego para cambiar entre modo light y modo oscuro.

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
 const telefono = document.getElementById("telefono");
 const submit = document.getElementById("submit");
-//const toggleButton = document.getElementById('toggle');
+const themeToggle = document.getElementById('theme-toggle');
 
 
 submit.addEventListener("click", function() {
@@ -105,12 +105,19 @@ submit.addEventListener("click", function() {
 
 
 
-/*
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
 });
-*/
 
-
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeToggle.checked = true;
+}
 
 
